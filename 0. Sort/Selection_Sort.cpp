d03bd89cab6bb3d0c365da3924ecb12edc045f02
Duy Son <uchihason5@gmail.com>
2025-07-25 16:18:21 +0700

    SelectionSort

--- 0. Sort/Selection_Sort.cpp
+++ 0. Sort/Selection_Sort.cpp
@@ -4,6 +4,17 @@ using namespace std;
 void selectionSort(int a[], int n){
     for(int i = 0; i < n - 1; ++i){
         int min_pos = i;
-        
+        for(int j = i + 1; j < n; ++j)
+            if(a[j] < a[min_pos]) 
+                min_pos = j;
+        swap (a[i], a[min_pos]);
     }
+}
+
+int main(){
+    int n; cin >> n;
+    int a[n];
+    for(int &x : a) cin >> x;
+    selectionSort(a, n);
+    for(int x : a) cout << x << " ";
 }
